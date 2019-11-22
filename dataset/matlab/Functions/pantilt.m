0b5634a69329028aac2582d01c47b393cf636e5f
Maximvda <maximvda123@gmail.com>
2019-11-22 14:27:39 +0100

    Added LED configuartion image in Media bracnh

--- dataset/matlab/Functions/pantilt.m
+++
@@ -1,7 +0,0 @@
-function  pantilt(tx_id,angle_polar,angle_azimuth,mx)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    fwrite(mx,[cast([5;tx_id;angle_polar;angle_azimuth],'uint8')],'uint8');
-end
-
--- dataset/matlab/Functions/shutdown_testbed.m
+++
@@ -1,3 +0,0 @@
-system('sh ~/ssh_scripts/turn_off_all.sh');
-% pause(5);
-% system('sh ~/ssh_scripts/shutdown_all.sh');
