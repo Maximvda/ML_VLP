9646674382ce7210bb9703361522dfc88739777c
Maximvda <maximvda123@gmail.com>
2019-12-18 18:44:36 +0100

    Bug fix

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,12 +6,7 @@ from torch.utils.data import Dataset
 
 #Expansion of the Dataset class to fit our dataset
 class data(Dataset):
-    def __init__(self, path, split, simulate):
-        if simulate:
-            str = path.split("/")[-1]
-            str = 'simulation_' + str
-            rest = "/".join(path.split("/")[:-1])
-            path = os.path.join(rest,str)
+    def __init__(self, path, split):
         #Open the file with data
         with open(path, 'rb') as f:
             self.data = pickle.load(f)[split]
--- dataset/setup_database.py
+++ dataset/setup_database.py
@@ -8,7 +8,8 @@ from simulation.simulation import testbed_simulation
 #Sets up a data loader for the requested split
 def setup_database(args, split="train"):
     #Choose correct file depending on desired split
-    file = '_'.join(('data', str(args.TX_config),str(args.TX_input), str(args.dynamic))) + '.data'
+    file = 'simulation_data' if args.simulate and split == 'train' else 'data'
+    file = '_'.join((file, str(args.TX_config),str(args.TX_input), str(args.dynamic))) + '.data'
     path = os.path.join(args.dataroot, file)
 
     if args.simulate:
@@ -20,7 +21,7 @@ def setup_database(args, split="train"):
         preprocess(args.dataroot, args.TX_config, args.TX_input, args.normalise, args.dynamic)
 
     #Initialise dataset and setup a data loader
-    dataset = data(path, split, args.simulate)
+    dataset = data(path, split)
     dataLoader = DataLoader(dataset, batch_size = args.batch_size, shuffle=True, num_workers=8)
 
     return dataLoader
