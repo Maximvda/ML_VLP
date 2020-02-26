d1332477e7625bcf751629554648bddb50af8d22
Maximvda <maximvda123@gmail.com>
2020-02-26 17:47:22 +0100

    Added data augmentation

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,8 +2,41 @@ import pickle
 import os
 import torch
 import numpy as np
+import random
 
 from torch.utils.data import Dataset
+
+def augmentation(input, output):
+    prob = random.random()
+    if 0.12 < prob < 0.65:
+        indices = np.random.choice(np.arange(9), replace=False, size=int(9*prob))
+        for ind in indices:
+            input[ind] = -1
+
+    prob = random.random()
+    #rotate data 25deg
+    if 0.25 < prob <= 0.5:
+        print('rotated 25 degrees')
+        output = [output[1], -output[0]]
+        input = [input[2], input[5], input[8],
+                input[1], input[4], input[7],
+                input[0], input[3], input[6]]
+    #rotate data 50 deg
+    elif prob <= 0.75:
+        print('rotated 50 degrees')
+        output = [-output[0], -output[1]]
+        input = [input[8], input[7], input[6],
+                input[5], input[4], input[3],
+                input[2], input[1], input[0]]
+    elif prob <= 1:
+        print('rotated 75 degrees')
+        output = [-output[1], output[0]]
+        input = [input[6], input[3], input[0],
+                input[7], input[4], input[1],
+                input[8], input[5], input[2]]
+    return input, output
+
+
 
 #Expansion of the Dataset class to fit our dataset
 class data(Dataset):
@@ -26,6 +59,7 @@ class data(Dataset):
         #Load a specific data item
         input = self.data[idx][0]
         output = self.data[idx][1]
+        input, output = augmentation(input, output)
 
         #Reshape the input into a square if the cnn model is used
         if not self.shape == None:
