b9e9e66bd48289fc9cff282b37b3bb9f57fffd85
Maximvda <maximvda123@gmail.com>
2020-04-20 17:34:46 +0200

    Bugfix experiment 1

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -4,18 +4,35 @@ import torch.multiprocessing as _mp
 from experiments.worker import Worker
 from utils.plotscript import *
 from eval.eval import Eval_obj
+from dataset.setup_database import setup_database
 
 def experiment(args):
     print("Running experiment {}".format(args.experiment))
     hyper_par = []
+
+    #Make sure the datasets are processed.
+    args.cell_type = "2x2"
+    setup_database(args)
+    args.cell_type = "3x3"
+    setup_database(args)
 
     #Experiment 1 looks at the influence on performance of different cell_types in function of the amount of blockage
     if args.experiment == 1:
         setup_dir(args, 'experiment_unit_cell_1')
         #Define the parameters to train models on
         for i in range(11):
-            hyper_par.append({'cell_type': '3x3', 'blockage': i*0.1})
-            hyper_par.append({'cell_type': '2x2', 'blockage': i*0.1})
+            hyper_par.append({'cell_type': '3x3', 'blockage': i*0.1,
+                                'dataset_path': {
+                                    'train': '/home/r0579568/ML_VLP/dataset/database/data_3x3_True_train.data',
+                                    'val': '/home/r0579568/ML_VLP/dataset/database/data_3x3_True_val.data',
+                                    'test': '/home/r0579568/ML_VLP/dataset/database/data_3x3_True_test.data',
+                                }})
+            hyper_par.append({'cell_type': '2x2', 'blockage': i*0.1,
+                                'dataset_path': {
+                                    'train': '/home/r0579568/ML_VLP/dataset/database/data_2x2_True_train.data',
+                                    'val': '/home/r0579568/ML_VLP/dataset/database/data_2x2_True_val.data',
+                                    'test': '/home/r0579568/ML_VLP/dataset/database/data_2x2_True_test.data',
+                                }})
 
         run_experiment(args, hyper_par)
 
