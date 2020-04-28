10624be069bd74b399ecc8413febd5a13c7fc92a
Maximvda <maximvda123@gmail.com>
2020-04-28 14:03:50 +0200

    Added results and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Device orientation from mobile application
+
+The code for the android application can be found in the zip file. This code can be adapted to setup a new experiment, for this adaptation have to be made to the backend cause a different server should be used to receive the logger files.
+The main python script includes the code to create three plots of the measured angles: azimuth, pitch and roll.
+
+## Results
+
+The number of users for every day during one week are listed in the table below.
+| | Day 1 | Day 2 | Day 3 | Day 4 | Day 5 | Day 6 | Day 7 |
+| --- | --- | --- | --- | --- | --- | --- | --- |
+|Users | 4 | 5 | 5 | 3 | 3 | 3 | 4 |
+
+Histograms for each angle can also be found below.
+<img src="https://github.com/Maximvda/ML_VLP/blob/deviceOrientation/result/azimut_week.png" width="512">
+<img src="https://github.com/Maximvda/ML_VLP/blob/deviceOrientation/result/pitch_week.png" width="512">
+<img src="https://github.com/Maximvda/ML_VLP/blob/deviceOrientation/result/roll_week.png" width="512">
--- main.py
+++ main.py
@@ -8,39 +8,67 @@ def main():
     result = os.path.join(os.getcwd(),'result')
     if not os.path.exists(result):
         os.mkdir(result)
-    files = os.listdir(root)
-    users = 0; id_list = []
-    azimut = [];    pitch = []; roll = []
-    for file in files:
-        id = file.split("_")[0]
-        if not (id in id_list):
-            users += 1
-            id_list.append(id)
-        with open(os.path.join(root,file), 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                azimut.append(float(row[0])*180/np.pi)
-                pitch.append(float(row[1])*180/np.pi)
-                roll.append(float(row[2])*180/np.pi)
+    folders = os.listdir(root)
 
-    print(users)
+    azimut = [];    pitch = []; roll = []
+    azimut_week = [];    pitch_week = []; roll_week = []
+    for folder in folders:
+        file_path = os.path.join(root,folder)
+        files = os.listdir(file_path)
+        users = 0; id_list = []
+        for file in files:
+            id = file.split("_")[0]
+            if not (id in id_list):
+                users += 1
+                id_list.append(id)
+            with open(os.path.join(file_path,file), 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    azimut.append(float(row[0])*180/np.pi)
+                    pitch.append(float(row[1])*180/np.pi)
+                    roll.append(float(row[2])*180/np.pi)
+                    if folder in ["day1", "day2", "day3", "day4", "day5", "day6", "day7"]:
+                        azimut_week.append(float(row[0])*180/np.pi)
+                        pitch_week.append(float(row[1])*180/np.pi)
+                        roll_week.append(float(row[2])*180/np.pi)
+
+        print("Number of users for {} is: {}".format(folder,users))
+
     plt.figure()
     plt.hist(azimut, density=True, bins=100, label="Azimut")
     plt.title("Distribution of Azimut angle.")
-    plt.xticks([-180,-90,0,90,180], ("", 'West', 'North', 'East', 'South'))
+    plt.xticks([-180,-90,0,90,180], ("South", 'West', 'North', 'East', 'South'))
     plt.savefig(os.path.join(result,'azimut.png'))
 
     plt.figure()
     plt.hist(pitch, density=True, bins=100, label="Pitch")
     plt.title("Distribution of Pitch angle.")
-    plt.xticks([-180,-90,0,90,180], ('-180°','-90°', '0°', '90°', '180°'))
+    plt.xticks([-90,-45,0,45,90], ('-90°','-45°', '0°', '45°', '90°'))
     plt.savefig(os.path.join(result,'pitch.png'))
 
     plt.figure()
-    plt.hist(pitch, density=True, bins=100, label="Roll")
+    plt.hist(roll, density=True, bins=100, label="Roll")
     plt.title("Distribution of Roll angle.")
-    plt.xticks([-90,-45,0,45,90], ('-90°','-45°', '0°', '45°', '90°'))
+    plt.xticks([-180,-90,0,90,180], ('-180°','-90°', '0°', '90°', '180°'))
     plt.savefig(os.path.join(result,'roll.png'))
+
+    plt.figure()
+    plt.hist(azimut_week, density=True, bins=100, label="Azimut")
+    plt.title("Distribution of Azimut angle.")
+    plt.xticks([-180,-90,0,90,180], ("South", 'West', 'North', 'East', 'South'))
+    plt.savefig(os.path.join(result,'azimut_week.png'))
+
+    plt.figure()
+    plt.hist(pitch_week, density=True, bins=100, label="Pitch")
+    plt.title("Distribution of Pitch angle.")
+    plt.xticks([-90,-45,0,45,90], ('-90°','-45°', '0°', '45°', '90°'))
+    plt.savefig(os.path.join(result,'pitch_week.png'))
+
+    plt.figure()
+    plt.hist(roll_week, density=True, bins=100, label="Roll")
+    plt.title("Distribution of Roll angle.")
+    plt.xticks([-180,-90,0,90,180], ('-180°','-90°', '0°', '90°', '180°'))
+    plt.savefig(os.path.join(result,'roll_week.png'))
 
 if __name__ == '__main__':
     main()
